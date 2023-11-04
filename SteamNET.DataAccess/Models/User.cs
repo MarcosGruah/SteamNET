fc4bb0a867fcdbfdd914fe468aaaaae3c6d04de1
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2023-11-04 03:20:17 -0300

    User Model for DataAccess and fixed some props on the User table

---
+++ SteamNET.DataAccess/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace SteamNET.DataAccess.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string SteamId { get; set; }
+        public string PersonaName { get; set; }
+        public string Avatar { get; set; }
+        public string AvatarMedium { get; set; }
+        public string AvatarFull { get; set; }
+        public DateTime TimeCreatedSteam { get; set; }
+        public DateTime TimeCreatedDb { get; set; }
+        public DateTime LastUpdateDb { get; set; }
+    }
+}

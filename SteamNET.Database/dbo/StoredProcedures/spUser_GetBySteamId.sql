4b83b06f48418a9e594ca48bbef2e188ce3bd478
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2023-11-04 02:58:12 -0300

    GetUserBySteamId Stored Procedure

---
+++ SteamNET.Database/dbo/StoredProcedures/spUser_GetBySteamId.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spUser_GetBySteamId]
+	@SteamId NVARCHAR(50)
+AS
+BEGIN
+	SELECT [Id], [SteamId], [PersonaName], [Avatar], [AvatarMedium], [AvatarFull], [TimeCreatedSteam], [TimeCreatedDb], [LastUpdateDb]
+	FROM [dbo].[User]
+	WHERE [SteamId] = @SteamId
+END

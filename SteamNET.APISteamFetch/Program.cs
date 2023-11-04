d0d2cd3182502ca6f685b723503d31c2029b1eef
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2023-11-04 06:19:11 -0300

    Setup for using secrets to access the value of the Steam WebApiKey

--- SteamNET.APISteamFetch/Program.cs
+++ SteamNET.APISteamFetch/Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
+var steamWebApiKey = builder.Configuration["Steam:WebApiKey"];
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
